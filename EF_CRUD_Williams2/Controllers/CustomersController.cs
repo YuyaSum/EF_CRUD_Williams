f515e5fa98b36d662e6f557f8f03f350b2e5dd39
richard williams <richard.williams@student.centralia.edu>
2022-03-13 19:32:03 -0700

    Add project files.

---
+++ EF_CRUD_Williams2/Controllers/CustomersController.cs
@@ -0,0 +1,204 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EF_CRUD_Williams2.Context;
+using EF_CRUD_Williams2.Models;
+
+namespace EF_CRUD_Williams2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly SalesContext _context;
+
+        public CustomersController(SalesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Customers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Customers.ToListAsync());
+        }
+
+        IEnumerable<Customer> Search(string search, string type) {
+
+            if (search != null)
+            {
+                switch (type)
+                {
+                    case "city":
+                        return _context.Customers.Where(c => c.City.ToLower() == search.ToLower());
+
+                    case "lastinitial":
+                        return _context.Customers.Where(c => c.LastName.ToLower().FirstOrDefault() ==
+                            search.ToLower().FirstOrDefault());
+
+                    case "lastname":
+                        return _context.Customers.Where(c => c.LastName.ToLower() == search.ToLower());
+
+                    default:
+                        return _context.Customers.ToList();
+                }
+            }
+            else { 
+                return _context.Customers.ToList();
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Index(string search, string type) {
+
+            //try catch here for error handling?
+
+            return View(Search(search, type));
+
+        }
+        
+        //public async Task<IActionResult> SearchView()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> SearchView(int id, [Bind("Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        //{
+        //    return View();
+        //}
+
+            // GET: Customers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // GET: Customers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                //in here maybe mess with id so it don't break?
+                //maybe make a proc that does MAXID+1 to have it not break
+                //can use adventures in crud to aid in this
+
+                _context.Add(customer);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,City,Country,Phone")] Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(int id)
+        {
+            return _context.Customers.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EF_CRUD_Williams2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EF_CRUD_Williams2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
